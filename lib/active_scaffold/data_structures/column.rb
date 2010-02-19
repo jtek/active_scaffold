bc2250481152d700a13c7c84f83aaf545a06a57f
Lionel Bouton <lionel.bouton@jtek.fr>
2010-02-19 21:50:21 +0100

    Revert change Workaround: declare virtual column with config.columns.add and assign label to it

--- lib/active_scaffold/data_structures/column.rb
+++ lib/active_scaffold/data_structures/column.rb
@@ -27,7 +27,7 @@ module ActiveScaffold::DataStructures
     # if left alone it will utilize human_attribute_name which includes localization
     attr_writer :label
     def label
-      as_(@label) || as_(active_record_class.human_attribute_name(name.to_s))
+      as_(@label) || active_record_class.human_attribute_name(name.to_s)
     end
 
     # a textual description of the column and its contents. this will be displayed with any associated form input widget, so you may want to consider adding a content example.
